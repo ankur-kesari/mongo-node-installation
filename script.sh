0b8fcc3a45ee421fd67bc8eb0a1ef6714d42c2d3
Ankur Kesari <64275046+ankur-kesari@users.noreply.github.com>
2022-03-29 10:58:44 +0530

    update mongodb script

--- script.sh
+++ script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-apt-get update
-apt-get install nodejs npm -y
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+apt-get update -y
 wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
+sudo systemctl enable mongod
